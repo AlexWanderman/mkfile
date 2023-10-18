1babbe86c2dc1216f6d5c30b99ff67f7bd618085
Your Name <you@example.com>
2023-10-18 18:25:21 +0300

    Added support for options: -v --verbose - print a message for each file; -p --parents - create parent directories recursively; -o --override - override already existing files.

--- readme.md
+++ readme.md
@@ -1,12 +1,39 @@
-mkfile file_path...
+# About the project
 
-Creates text files. For each file outputs a success message or an error message. By default overrides the existing files (for now).
+Minimal Rust CLI app with no external dependencies. Creates text files. May
+create parent directories recursively, override existing files and output
+verbosely.
 
-TODO:
+# Description
+
+mkfile \[OPTION\]... \[PATH\]...
+
+-v --verbose - print a message for each file;
+-p --parents - create parent directories recursively;
+-o --override - override already existing files;
+
+# Usage example
+
+Create a bunch of files in verbose mode. Some of the files couldn't be created because we didn't include -p (--parent) option to create parent directories.
+```
+$ mkfile -v /file.txt /test/file.txt /home/user/file.txt /home/user/test/file.txt
+
+/file.txt: Permission denied (os error 13)
+/test/file.txt: No such file or directory (os error 2)
+/home/user/file.txt: Created
+/home/user/test/file.txt: No such file or directory (os error 2)
+```
+
+We failed to create a file because it already exist, but we didn't include -o (--override) option to override it.
+```
+$ mkfile -v ~/file.txt
+
+/home/user/file.txt: File already exist
+```
+
+# TODO
+
 - Add parameters:
-- -p --parents     - create parent directories
-- -o --override    - override the existing files
-- -v --verbose     - print a message for each file
 - -T --text=string - include this text in all new files
 - --help
 - --version
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,101 @@
+use std::collections::VecDeque;
 use std::env;
+use std::fs;
 use std::fs::File;
+use std::path::Path;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // Collect args and remove the executable path
+    let mut args: VecDeque<String> = env::args().collect();
+    args.pop_front();
 
-    if args.len() == 1 {
+    // Return if no args
+    if args.is_empty() {
         return;
     }
 
-    for arg in args[1..].iter() {
-        match File::create(arg) {
-            Ok(_) => println!("{arg}: Created"),
-            Err(e) => println!("{arg}: {e}"),
+    // Options and paths
+    let mut options = Vec::<String>::new();
+    let mut options_wrong = Vec::<String>::new();
+    let mut paths = Vec::<&Path>::new();
+
+    let mut is_verbose = false;
+    let mut create_parents = false;
+    let mut do_override = false;
+
+    for arg in &args {
+        let a = arg.chars().nth(0);
+        let b = arg.chars().nth(1);
+        let c = arg.chars().nth(3);
+
+        // Long option --option
+        if a.eq(&Some('-')) && b.eq(&Some('-')) && c.is_some() {
+            options.push(arg[2..].to_string());
+        }
+        // Short option -o (single) or -vao (multiple)
+        else if a.eq(&Some('-')) && b.ne(&Some('-')) {
+            for opt in arg.chars() {
+                match opt {
+                    '-' => {}
+                    _ => options.push(opt.to_string()),
+                }
+            }
+        }
+        // Paths
+        else {
+            paths.push(Path::new(arg));
+        }
+    }
+
+    // Proceed options
+    for opt in &options {
+        match opt.as_str() {
+            "v" | "verbose" => is_verbose = true,
+            "p" | "parents" => create_parents = true,
+            "o" | "override" => do_override = true,
+            _ => options_wrong.push(opt.to_string()),
+        }
+    }
+
+    // Output wrong options and return
+    if !options_wrong.is_empty() {
+        let opt_list = options_wrong.join(", ");
+
+        if options_wrong.len() == 1 {
+            eprintln!("Wrong option: {opt_list}");
+        } else {
+            eprintln!("Wrong options: {opt_list}");
+        }
+
+        return;
+    }
+
+    for path in paths {
+        // Skip file if exist, but do_override is false
+        if path.exists() && !do_override {
+            println!("{}: File already exist", path.display());
+            continue;
+        }
+
+        // Create parent directories
+        if create_parents {
+            let parent = path.parent().unwrap_or(Path::new(""));
+            if !parent.exists() {
+                match fs::create_dir_all(parent) {
+                    Err(e) if is_verbose => {
+                        println!("{}: {}", path.display(), e);
+                        continue;
+                    }
+                    _ => {}
+                };
+            }
+        }
+
+        // Create files
+        match File::create(path) {
+            Ok(_) if is_verbose => println!("{}: Created", path.display()),
+            Err(e) if is_verbose => println!("{}: {}", path.display(), e),
+            _ => {}
         };
     }
 }
