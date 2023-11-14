e5d87372cde918a362f77dafd0b567c7fa92004a
Alex Wanderman <58393741+AlexWanderman@users.noreply.github.com>
2023-11-14 18:35:15 +0300

    Version 0.3.0 changes - Main function rework - New options were added: dry run and default text - README file renamed

--- src/main.rs
+++ src/main.rs
@@ -1,110 +1,171 @@
-use std::env;
-use std::fs;
-use std::fs::File;
-use std::path::Path;
+use std::{env, fs, path::Path};
 
 fn main() {
-    // Collect args
+    // Stage 0: args
     let args: Vec<String> = env::args().collect();
 
-    // Return if no args and print help message
-    if args.len() <= 1 {
-        print_help_msg();
+    // Stage 1: options
+    let mut do_dry_run = false;
+    let mut do_verbose_output = false;
+    let mut do_create_parents = false;
+    let mut do_overwrite = false;
+    let mut default_text = String::new();
+
+    let mut wrong_options = Vec::<String>::new();
+    let mut paths = Vec::<String>::new();
+
+    // Using an iterator to peek the next value inside a loop. Skipping
+    // the path of the executable (with .next()).
+    let mut args_iter = args.iter();
+    args_iter.next();
+
+    while let Some(arg) = args_iter.next() {
+        let options = if arg.chars().all(|x| x.eq(&'-')) {
+            // - or -- are wrong options
+            wrong_options.push(arg.to_string());
+            continue;
+        } else if arg.starts_with("--") {
+            // Single long option
+            vec![arg.strip_prefix("--").unwrap().to_string()]
+        } else if arg.starts_with("-") {
+            // Single or multiple short options
+            arg.strip_prefix("-")
+                .unwrap()
+                .chars()
+                .map(|x| x.to_string())
+                .collect()
+        } else {
+            // Not an option
+            paths.push(arg.to_string());
+            continue;
+        };
+
+        for option in options {
+            match option.as_str() {
+                "d" | "dry" => do_dry_run = true,
+                "v" | "verbose" => do_verbose_output = true,
+                "p" | "parents" => do_create_parents = true,
+                "o" | "overwrite" => do_overwrite = true,
+                "T" | "text" => {
+                    // Default text was already written
+                    if !default_text.is_empty() {
+                        eprintln!("Error! Multiple default text options are not supported.");
+                        return;
+                    }
+
+                    let text = args_iter.next();
+
+                    // Default text was not provided
+                    if text.is_none() {
+                        eprintln!("Error! Default text was not provided.");
+                        return;
+                    }
+
+                    default_text = text.unwrap().to_string() + "\n";
+                }
+                "help" => {
+                    print_help_msg();
+                    return;
+                }
+                "version" => {
+                    print_version_msg();
+                    return;
+                }
+                _ => wrong_options.push(option),
+            }
+        }
+    }
+
+    if !wrong_options.is_empty() {
+        if wrong_options.len() == 1 {
+            let option = wrong_options.first().unwrap();
+            eprintln!("Error! Wrong option: {option}.");
+        } else {
+            let options = wrong_options.join(", ");
+            eprintln!("Error! Wrong options: {options}.");
+        }
         return;
     }
 
-    // Options and paths
-    let mut options = Vec::<String>::new();
-    let mut options_wrong = Vec::<String>::new();
-    let mut paths = Vec::<&Path>::new();
-
-    let mut is_verbose = false;
-    let mut create_parents = false;
-    let mut do_override = false;
-
-    // [1..] since the first arg is the executable path
-    for arg in &args[1..] {
-        let a = arg.chars().nth(0);
-        let b = arg.chars().nth(1);
-        let c = arg.chars().nth(2);
-
-        // Long option --option
-        if a.eq(&Some('-')) && b.eq(&Some('-')) && c.is_some() {
-            options.push(arg[2..].to_string());
-        }
-        // Short option -o (single) or -vao (multiple)
-        else if a.eq(&Some('-')) && b.ne(&Some('-')) {
-            for opt in arg.chars() {
-                match opt {
-                    '-' => {}
-                    _ => options.push(opt.to_string()),
-                }
-            }
-        }
-        // Paths
-        else {
-            paths.push(Path::new(arg));
-        }
-    }
-
-    // Proceed options
-    for opt in &options {
-        match opt.as_str() {
-            "v" | "verbose" => is_verbose = true,
-            "p" | "parents" => create_parents = true,
-            "o" | "override" => do_override = true,
-            "help" => {
-                print_help_msg();
-                return;
-            }
-            "version" => {
-                print_version_msg();
-                return;
-            }
-            _ => options_wrong.push(opt.to_string()),
-        }
-    }
-
-    // Output wrong options and return
-    if !options_wrong.is_empty() {
-        let opt_list = options_wrong.join(", ");
-
-        if options_wrong.len() == 1 {
-            eprintln!("Wrong option: {opt_list}");
-        } else {
-            eprintln!("Wrong options: {opt_list}");
-        }
-
+    // Stage 2: files
+    if paths.is_empty() {
+        println!("Warning! No files were provided. To get a hint use --help.\n");
         return;
     }
 
     for path in paths {
-        // Skip file if exist, but do_override is false
-        if path.exists() && !do_override {
-            println!("{}: File already exist", path.display());
-            continue;
-        }
-
-        // Create parent directories
-        if create_parents {
-            let parent = path.parent().unwrap_or(Path::new(""));
-            if !parent.exists() {
-                match fs::create_dir_all(parent) {
-                    Err(e) if is_verbose => {
-                        println!("{}: {}", path.display(), e);
-                        continue;
-                    }
-                    _ => {}
-                };
-            }
-        }
-
-        // Create files
-        match File::create(path) {
-            Ok(_) if is_verbose => println!("{}: Created", path.display()),
-            Err(e) if is_verbose => println!("{}: {}", path.display(), e),
-            _ => {}
+        let full_path_str = if path.starts_with("/") {
+            path.to_string()
+        } else {
+            let mut full_path = env::current_dir().unwrap();
+            full_path.push(&path);
+            full_path.display().to_string()
         };
+
+        let full_path = Path::new(&full_path_str);
+        let full_parent_path = full_path.parent().unwrap_or(Path::new("/"));
+
+        let mut is_overwritten = false;
+        let mut is_parented = false;
+
+        // Overwritten file
+        match full_path.try_exists() {
+            Ok(true) => {
+                if !do_overwrite {
+                    eprintln!("{}: Already exist", &full_path_str);
+                    continue;
+                }
+                is_overwritten = true;
+            }
+            Err(e) if do_verbose_output => eprintln!("{}: {}", &full_path_str, e),
+            _ => {}
+        }
+
+        // New file with parent
+        match full_parent_path.try_exists() {
+            Ok(false) => {
+                if !do_create_parents {
+                    eprintln!("{}: Parent does not exist", &full_path_str);
+                    continue;
+                }
+
+                if !do_dry_run {
+                    match fs::create_dir_all(&full_parent_path) {
+                        Err(e) if do_verbose_output => eprintln!("{}: {}", &full_path_str, e),
+                        _ => {}
+                    }
+                }
+
+                is_parented = true;
+            }
+            Err(e) if do_verbose_output => eprintln!("{}: {}", &full_path_str, e),
+            _ => {}
+        }
+
+        // New file
+        if !do_dry_run {
+            match fs::write(&full_path, &default_text) {
+                Ok(()) if do_verbose_output => {
+                    let created = if is_overwritten {
+                        "Overwritten"
+                    } else {
+                        "Created"
+                    };
+                    let parented = if is_parented { "with parent" } else { "" };
+                    println!("{}: {} {}", &full_path_str, created, parented);
+                }
+                Err(e) if do_verbose_output => eprintln!("{}: {}", &full_path_str, e),
+                _ => {}
+            }
+        } else {
+            let created = if is_overwritten {
+                "overwritten"
+            } else {
+                "created"
+            };
+            let parented = if is_parented { "with parent" } else { "" };
+            println!("{}: To be {} {}", &full_path_str, created, parented);
+        }
     }
 }
 
@@ -112,11 +173,13 @@ fn print_help_msg() {
     println!("Usage: mkfile [OPTION]... PATH...");
     println!("Create file(s), if they do not already exist.\n");
     println!("Options:");
-    println!("-v --verbose    print a message for each file");
-    println!("-p --parents    create parent directories recursively");
-    println!("-o --override   override already existing files");
-    println!("   --help       display this help and exit");
-    println!("   --version    output version information and exit (todo!)");
+    println!("-d --dry            perform \"dry\" run, always verbose");
+    println!("-v --verbose        print a message for each file");
+    println!("-p --parents        create parent directories recursively");
+    println!("-o --overwrite      overwrite already existing files");
+    println!("-T --text \"STRING\"  default text for every file");
+    println!("   --help           display this help and exit");
+    println!("   --version        output version information and exit");
 }
 
 fn print_version_msg() {
